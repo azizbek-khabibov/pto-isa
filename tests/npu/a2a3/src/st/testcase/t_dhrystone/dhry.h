3c61fc7cb792b59d5ecfb2756f5df493de436491
Manto_Yung <yung.man.to@huawei.com>
2026-04-01 09:27:13 +0800

    Revise dhrystone ST copyright declaration
    Co-authored-by: Manto_Yung<yung.man.to@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !649 merge drhystone into master
    
    Revise dhrystone ST copyright declaration
    
    Created-by: Manto_Yung
    Commit-by: Manto_Yung
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：Revise copyright declaration
    
    
    
    
    See merge request: cann/pto-isa!649

--- tests/npu/a2a3/src/st/testcase/t_dhrystone/dhry.h
+++ tests/npu/a2a3/src/st/testcase/t_dhrystone/dhry.h
@@ -6,6 +6,12 @@ Please refer to the License for details. You may not use this file except in com
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
 INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 See LICENSE in the root of the software repository for the full text of the License.
+
+The Dhrystone benchmark code is based on:
+"Dhystone" Benchmark Program, C Version 2.1
+Author: Reinhold P. Weicker, Siemens AG (1984, 1988)
+Published in "Communications of the ACM" vol. 27, no. 10 (Oct. 1984)
+This is a freely distributable industry-standard benchmark.
 */
 #include <stdio.h>
 #include <pto/common/constants.hpp>
@@ -16,70 +22,41 @@ See LICENSE in the root of the software repository for the full text of the Lice
 #define structassign(d, s) d = s
 #endif
 
-#ifdef NOENUM
-#define Ident_1 0
-#define Ident_2 1
-#define Ident_3 2
-#define Ident_4 3
-#define Ident_5 4
-typedef int Enumeration;
-#else
-typedef enum
+enum Enumeration
 {
     Ident_1,
     Ident_2,
     Ident_3,
     Ident_4,
     Ident_5
-} Enumeration;
-#endif
-/* for boolean and enumeration types in Ada, Pascal */
+};
 
-/* General definitions: */
-#define Null 0
-/* Value of a Null pointer */
-#define true 1
-#define false 0
-
-typedef int One_Thirty;
-typedef int One_Fifty;
-typedef char Capital_Letter;
-typedef int Boolean;
 typedef char Str_30[31];
-typedef int Arr_1_Dim[50];
-typedef int Arr_2_Dim[50][50];
 
 typedef struct record {
-    struct record *Ptr_Comp;
-    Enumeration Discr;
+    struct record *ptr_comp;
+    Enumeration discr;
     union {
         struct {
-            Enumeration Enum_Comp;
-            int Int_Comp;
-            char Str_Comp[31];
+            Enumeration enum_comp;
+            int int_comp;
+            char str_comp[31];
         } var_1;
-        struct {
-            Enumeration E_Comp_2;
-            char Str_2_Comp[31];
-        } var_2;
-        struct {
-            char Ch_1_Comp;
-            char Ch_2_Comp;
-        } var_3;
     } variant;
 } Rec_Type, *Rec_Pointer;
 
-AICORE void Proc_1(Rec_Pointer Ptr_Val_Par);
-AICORE void Proc_2(One_Fifty *Int_Par_Ref);
-AICORE void Proc_3(Rec_Pointer *Ptr_Ref_Par);
-AICORE void Proc_4(void);
-AICORE void Proc_5(void);
-AICORE void Proc_6(Enumeration Enum_Val_Par, Enumeration *Enum_Ref_Par);
-AICORE void Proc_7(One_Fifty Int_1_Par_Val, One_Fifty Int_2_Par_Val, One_Fifty *Int_Par_Ref);
-AICORE void Proc_8(int *Arr_1_Par_Ref, int *Arr_2_Par_Ref, int Int_1_Par_Val, int Int_2_Par_Val);
-AICORE Enumeration Func_1(Capital_Letter Ch_1_Par_Val, Capital_Letter Ch_2_Par_Val);
-AICORE Boolean Func_2(Str_30 Str_1_Par_Ref, Str_30 Str_2_Par_Ref);
-AICORE Boolean Func_3(Enumeration Enum_Par_Val);
-AICORE void memcpy(char *d, char *s, int l);
-AICORE void strcpy(char *des, char *source);
+AICORE void custom_strcpy(char *des, char *source);
 AICORE int strcmp(char *sl, char *s2);
+
+AICORE void proc_1(Rec_Pointer ptr_val_par);
+AICORE void proc_2(int *int_par_ref);
+AICORE void proc_3(Rec_Pointer *ptr_ref_par);
+AICORE void proc_4(void);
+AICORE void proc_5(void);
+AICORE void proc_6(Enumeration enum_val_par, Enumeration *enum_ref_par);
+AICORE void proc_7(int int_1_par_val, int int_2_par_val, int *int_par_ref);
+AICORE void proc_8(int *arr_1_par_ref, int *arr_2_par_ref, int int_1_par_val, int int_2_par_val);
+
+AICORE Enumeration func_1(char ch_1_par_val, char ch_2_par_val);
+AICORE bool func_2(Str_30 str_1_par_ref, Str_30 str_2_par_ref);
+AICORE bool func_3(Enumeration enum_par_val);
--- tests/npu/a2a3/src/st/testcase/t_dhrystone/t_dhrystone_kernel.cpp
+++ tests/npu/a2a3/src/st/testcase/t_dhrystone/t_dhrystone_kernel.cpp
@@ -6,20 +6,23 @@ Please refer to the License for details. You may not use this file except in com
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
 INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 See LICENSE in the root of the software repository for the full text of the License.
-*/
 
+The code snippet comes from zpu.
+
+Copyright Zylin AS. All rights reserved.
+
+The Dhrystone benchmark code is based on:
+"Dhystone" Benchmark Program, C Version 2.1
+Author: Reinhold P. Weicker, Siemens AG (1984, 1988)
+Published in "Communications of the ACM" vol. 27, no. 10 (Oct. 1984)
+This is a freely distributable industry-standard benchmark.
+*/
 #include "acl/acl.h"
 #include "dhry.h"
 
 using namespace pto;
 
-AICORE void memcpy(char *d, char *s, int l)
-{
-    while (l--)
-        *d++ = *s++;
-}
-
-AICORE void strcpy(char *des, char *source)
+AICORE void custom_strcpy(char *des, char *source)
 {
     char *r = des;
 
@@ -38,234 +41,234 @@ AICORE int strcmp(char *sl, char *s2)
     return ((*(unsigned char *)sl < *(unsigned char *)s2) ? -1 : +1);
 }
 
-AICORE void Proc_1(Rec_Pointer Ptr_Val_Par)
+AICORE void proc_1(Rec_Pointer ptr_val_par)
 {
-    Rec_Pointer Ptr_Glob = (Rec_Pointer)0x100000;
-    Rec_Pointer Next_Record = Ptr_Val_Par->Ptr_Comp;
-    /* == Ptr_Glob_Next */
-    /* Local variable, initialized with Ptr_Val_Par->Ptr_Comp,    */
+    Rec_Pointer ptr_glob = (Rec_Pointer)0x100000;
+    Rec_Pointer next_record = ptr_val_par->ptr_comp;
+    /* == ptr_glob_Next */
+    /* Local variable, initialized with ptr_val_par->ptr_comp,    */
     /* corresponds to "rename" in Ada, "with" in Pascal           */
 
-    structassign(*Ptr_Val_Par->Ptr_Comp, *Ptr_Glob);
-    Ptr_Val_Par->variant.var_1.Int_Comp = 5;
-    Next_Record->variant.var_1.Int_Comp = Ptr_Val_Par->variant.var_1.Int_Comp;
-    Next_Record->Ptr_Comp = Ptr_Val_Par->Ptr_Comp;
-    Proc_3(&Next_Record->Ptr_Comp);
-    /* Ptr_Val_Par->Ptr_Comp->Ptr_Comp
-                        == Ptr_Glob->Ptr_Comp */
-    if (Next_Record->Discr == Ident_1)
+    structassign(*ptr_val_par->ptr_comp, *ptr_glob);
+    ptr_val_par->variant.var_1.int_comp = 5;
+    next_record->variant.var_1.int_comp = ptr_val_par->variant.var_1.int_comp;
+    next_record->ptr_comp = ptr_val_par->ptr_comp;
+    proc_3(&next_record->ptr_comp);
+    /* ptr_val_par->ptr_comp->ptr_comp
+                        == ptr_glob->ptr_comp */
+    if (next_record->discr == Ident_1)
     /* then, executed */
     {
-        Next_Record->variant.var_1.Int_Comp = 6;
-        Proc_6(Ptr_Val_Par->variant.var_1.Enum_Comp, &Next_Record->variant.var_1.Enum_Comp);
-        Next_Record->Ptr_Comp = Ptr_Glob->Ptr_Comp;
-        Proc_7(Next_Record->variant.var_1.Int_Comp, 10, &Next_Record->variant.var_1.Int_Comp);
+        next_record->variant.var_1.int_comp = 6;
+        proc_6(ptr_val_par->variant.var_1.enum_comp, &next_record->variant.var_1.enum_comp);
+        next_record->ptr_comp = ptr_glob->ptr_comp;
+        proc_7(next_record->variant.var_1.int_comp, 10, &next_record->variant.var_1.int_comp);
     } else /* not executed */
-        structassign(*Ptr_Val_Par, *Ptr_Val_Par->Ptr_Comp);
-} /* Proc_1 */
+        structassign(*ptr_val_par, *ptr_val_par->ptr_comp);
+} /* proc_1 */
 
-AICORE void Proc_2(One_Fifty *Int_Par_Ref)
+AICORE void proc_2(int *int_par_ref)
 
 {
-    char *Ch_1_Glob = (char *)0x100110;
-    char *Int_Glob = (char *)0x100100;
-    One_Fifty Int_Loc;
-    Enumeration Enum_Loc;
+    char *ch_1_glob = (char *)0x100110;
+    char *int_glob = (char *)0x100100;
+    int int_loc;
+    Enumeration enum_Loc;
 
-    Int_Loc = *Int_Par_Ref + 10;
+    int_loc = *int_par_ref + 10;
     do /* executed once */
-        if (*Ch_1_Glob == 'A')
+        if (*ch_1_glob == 'A')
         /* then, executed */
         {
-            Int_Loc -= 1;
-            *Int_Par_Ref = Int_Loc - *Int_Glob;
-            Enum_Loc = Ident_1;
+            int_loc -= 1;
+            *int_par_ref = int_loc - *int_glob;
+            enum_Loc = Ident_1;
         }
-    while (Enum_Loc != Ident_1); /* true */
-} /* Proc_2 */
+    while (enum_Loc != Ident_1); /* true */
+} /* proc_2 */
 
-AICORE void Proc_3(Rec_Pointer *Ptr_Ref_Par)
+AICORE void proc_3(Rec_Pointer *ptr_ref_par)
 
 {
-    Rec_Pointer Ptr_Glob = (Rec_Pointer)0x100000;
-    int *Int_Glob = (int *)0x100100;
-    if (Ptr_Glob != Null)
+    Rec_Pointer ptr_glob = (Rec_Pointer)0x100000;
+    int *int_glob = (int *)0x100100;
+    if (ptr_glob != NULL)
         /* then, executed */
-        *Ptr_Ref_Par = Ptr_Glob->Ptr_Comp;
-    Proc_7(10, *Int_Glob, &Ptr_Glob->variant.var_1.Int_Comp);
-} /* Proc_3 */
+        *ptr_ref_par = ptr_glob->ptr_comp;
+    proc_7(10, *int_glob, &ptr_glob->variant.var_1.int_comp);
+} /* proc_3 */
 
-AICORE void Proc_4(void)
+AICORE void proc_4(void)
 {
-    Boolean Bool_Loc;
+    bool bool_Loc;
 
-    Boolean *Bool_Glob = (Boolean *)0x100108;
-    char *Ch_1_Glob = (char *)0x100110;
-    char *Ch_2_Glob = (char *)0x100118;
+    bool *bool_glob = (bool *)0x100108;
+    char *ch_1_glob = (char *)0x100110;
+    char *ch_2_glob = (char *)0x100118;
 
-    Bool_Loc = *Ch_1_Glob == 'A';
-    *Bool_Glob = Bool_Loc | *Bool_Glob;
-    *Ch_2_Glob = 'B';
-} /* Proc_4 */
+    bool_Loc = *ch_1_glob == 'A';
+    *bool_glob = bool_Loc | *bool_glob;
+    *ch_2_glob = 'B';
+} /* proc_4 */
 
-AICORE void Proc_5(void)
+AICORE void proc_5(void)
 {
-    Boolean *Bool_Glob = (Boolean *)0x100108;
-    char *Ch_1_Glob = (char *)0x100110;
+    bool *bool_glob = (bool *)0x100108;
+    char *ch_1_glob = (char *)0x100110;
 
-    *Ch_1_Glob = 'A';
-    *Bool_Glob = false;
-} /* Proc_5 */
+    *ch_1_glob = 'A';
+    *bool_glob = false;
+} /* proc_5 */
 
-AICORE void Proc_6(Enumeration Enum_Val_Par, Enumeration *Enum_Ref_Par)
+AICORE void proc_6(Enumeration enum_val_par, Enumeration *enum_ref_par)
 {
-    char *Int_Glob = (char *)0x100100;
-    *Enum_Ref_Par = Enum_Val_Par;
-    if (!Func_3(Enum_Val_Par))
+    char *int_glob = (char *)0x100100;
+    *enum_ref_par = enum_val_par;
+    if (!func_3(enum_val_par))
         /* then, not executed */
-        *Enum_Ref_Par = Ident_4;
-    switch (Enum_Val_Par) {
+        *enum_ref_par = Ident_4;
+    switch (enum_val_par) {
         case Ident_1:
-            *Enum_Ref_Par = Ident_1;
+            *enum_ref_par = Ident_1;
             break;
         case Ident_2:
-            if (*Int_Glob > 100)
+            if (*int_glob > 100)
                 /* then */
-                *Enum_Ref_Par = Ident_1;
+                *enum_ref_par = Ident_1;
             else
-                *Enum_Ref_Par = Ident_4;
+                *enum_ref_par = Ident_4;
             break;
         case Ident_3: /* executed */
-            *Enum_Ref_Par = Ident_2;
+            *enum_ref_par = Ident_2;
             break;
         case Ident_4:
             break;
         case Ident_5:
-            *Enum_Ref_Par = Ident_3;
+            *enum_ref_par = Ident_3;
             break;
     } /* switch */
-} /* Proc_6 */
+} /* proc_6 */
 
-AICORE void Proc_7(One_Fifty Int_1_Par_Val, One_Fifty Int_2_Par_Val, One_Fifty *Int_Par_Ref)
+AICORE void proc_7(int int_1_par_val, int int_2_par_val, int *int_par_ref)
 {
-    One_Fifty Int_Loc;
+    int int_loc;
 
-    Int_Loc = Int_1_Par_Val + 2;
-    *Int_Par_Ref = Int_2_Par_Val + Int_Loc;
-} /* Proc_7 */
+    int_loc = int_1_par_val + 2;
+    *int_par_ref = int_2_par_val + int_loc;
+} /* proc_7 */
 
-AICORE void Proc_8(int *Arr_1_Par_Ref, int *Arr_2_Par_Ref, int Int_1_Par_Val, int Int_2_Par_Val)
+AICORE void proc_8(int *arr_1_par_ref, int *arr_2_par_ref, int int_1_par_val, int int_2_par_val)
 {
-    One_Fifty Int_Index;
-    One_Fifty Int_Loc;
-    char *Int_Glob = (char *)0x100100;
+    int int_index;
+    int int_loc;
+    char *int_glob = (char *)0x100100;
 
-    Int_Loc = Int_1_Par_Val + 5;
-    *(Arr_1_Par_Ref + Int_Loc) = Int_2_Par_Val;
-    *(Arr_1_Par_Ref + Int_Loc + 1) = *(Arr_1_Par_Ref + Int_Loc);
-    *(Arr_1_Par_Ref + Int_Loc + 30) = Int_Loc;
-    for (Int_Index = Int_Loc; Int_Index <= Int_Loc + 1; ++Int_Index)
-        *(Arr_2_Par_Ref + Int_Loc * 50 + Int_Index) = Int_Loc;
-    (*(Arr_2_Par_Ref + Int_Loc * 50 + Int_Loc - 1)) += 1;
-    *(Arr_2_Par_Ref + (Int_Loc + 20) * 50 + Int_Loc) = *(Arr_1_Par_Ref + Int_Loc);
-    *Int_Glob = 5;
-} /* Proc_8 */
+    int_loc = int_1_par_val + 5;
+    *(arr_1_par_ref + int_loc) = int_2_par_val;
+    *(arr_1_par_ref + int_loc + 1) = *(arr_1_par_ref + int_loc);
+    *(arr_1_par_ref + int_loc + 30) = int_loc;
+    for (int_index = int_loc; int_index <= int_loc + 1; ++int_index)
+        *(arr_2_par_ref + int_loc * 50 + int_index) = int_loc;
+    (*(arr_2_par_ref + int_loc * 50 + int_loc - 1)) += 1;
+    *(arr_2_par_ref + (int_loc + 20) * 50 + int_loc) = *(arr_1_par_ref + int_loc);
+    *int_glob = 5;
+} /* proc_8 */
 
-AICORE Enumeration Func_1(Capital_Letter Ch_1_Par_Val, Capital_Letter Ch_2_Par_Val)
+AICORE Enumeration func_1(char ch_1_par_val, char ch_2_par_val)
 /*************************************************/
 /* executed three times                                         */
-/* first call:      Ch_1_Par_Val == 'H', Ch_2_Par_Val == 'R'    */
-/* second call:     Ch_1_Par_Val == 'A', Ch_2_Par_Val == 'C'    */
-/* third call:      Ch_1_Par_Val == 'B', Ch_2_Par_Val == 'C'    */
+/* first call:      ch_1_par_val == 'H', ch_2_par_val == 'R'    */
+/* second call:     ch_1_par_val == 'A', ch_2_par_val == 'C'    */
+/* third call:      ch_1_par_val == 'B', ch_2_par_val == 'C'    */
 {
-    Capital_Letter Ch_1_Loc;
-    Capital_Letter Ch_2_Loc;
-    char *Ch_1_Glob = (char *)0x100110;
+    char ch_1_loc;
+    char ch_2_loc;
+    char *ch_1_glob = (char *)0x100110;
 
-    Ch_1_Loc = Ch_1_Par_Val;
-    Ch_2_Loc = Ch_1_Loc;
-    if (Ch_2_Loc != Ch_2_Par_Val)
+    ch_1_loc = ch_1_par_val;
+    ch_2_loc = ch_1_loc;
+    if (ch_2_loc != ch_2_par_val)
         /* then, executed */
         return (Ident_1);
     else /* not executed */
     {
-        *Ch_1_Glob = Ch_1_Loc;
+        *ch_1_glob = ch_1_loc;
         return (Ident_2);
     }
-} /* Func_1 */
+} /* func_1 */
 
-AICORE Boolean Func_2(Str_30 Str_1_Par_Ref, Str_30 Str_2_Par_Ref)
+AICORE bool func_2(Str_30 str_1_par_ref, Str_30 str_2_par_ref)
 /*************************************************/
 /* executed once */
-/* Str_1_Par_Ref == "DHRYSTONE PROGRAM, 1'ST STRING" */
-/* Str_2_Par_Ref == "DHRYSTONE PROGRAM, 2'ND STRING" */
+/* str_1_par_ref == "DHRYSTONE PROGRAM, 1'ST STRING" */
+/* str_2_par_ref == "DHRYSTONE PROGRAM, 2'ND STRING" */
 {
-    One_Thirty Int_Loc;
-    Capital_Letter Ch_Loc;
-    int *Int_Glob = (int *)0x100100;
+    int int_loc;
+    char ch_loc;
+    int *int_glob = (int *)0x100100;
 
-    Int_Loc = 2;
-    while (Int_Loc <= 2) /* loop body executed once */
-        if (Func_1(Str_1_Par_Ref[Int_Loc], Str_2_Par_Ref[Int_Loc + 1]) == Ident_1)
+    int_loc = 2;
+    while (int_loc <= 2) /* loop body executed once */
+        if (func_1(str_1_par_ref[int_loc], str_2_par_ref[int_loc + 1]) == Ident_1)
         /* then, executed */
         {
-            Ch_Loc = 'A';
-            Int_Loc += 1;
+            ch_loc = 'A';
+            int_loc += 1;
         } /* if, while */
-    if (Ch_Loc >= 'W' && Ch_Loc < 'Z')
+    if (ch_loc >= 'W' && ch_loc < 'Z')
         /* then, not executed */
-        Int_Loc = 7;
-    if (Ch_Loc == 'R')
+        int_loc = 7;
+    if (ch_loc == 'R')
         /* then, not executed */
-        return (true);
+        return true;
     else /* executed */
     {
-        if (strcmp(Str_1_Par_Ref, Str_2_Par_Ref) > 0)
+        if (strcmp(str_1_par_ref, str_2_par_ref) > 0)
         /* then, not executed */
         {
-            Int_Loc += 7;
-            *Int_Glob = Int_Loc;
-            return (true);
+            int_loc += 7;
+            *int_glob = int_loc;
+            return true;
         } else /* executed */
-            return (false);
-    } /* if Ch_Loc */
-} /* Func_2 */
+            return false;
+    } /* if ch_loc */
+} /* func_2 */
 
-AICORE Boolean Func_3(Enumeration Enum_Par_Val)
+AICORE bool func_3(Enumeration enum_par_val)
 /***************************/
 /* executed once        */
-/* Enum_Par_Val == Ident_3 */
+/* enum_par_val == Ident_3 */
 {
-    Enumeration Enum_Loc;
+    Enumeration enum_Loc;
 
-    Enum_Loc = Enum_Par_Val;
-    if (Enum_Loc == Ident_3)
+    enum_Loc = enum_par_val;
+    if (enum_Loc == Ident_3)
         /* then, executed */
-        return (true);
+        return true;
     else /* not executed */
-        return (false);
-} /* Func_3 */
+        return false;
+} /* func_3 */
 
 template <int iteration>
 __global__ AICORE void runTDhrystone()
 {
-    One_Fifty Int_1_Loc;
-    One_Fifty Int_2_Loc;
-    One_Fifty Int_3_Loc;
-    char Ch_Index;
-    Enumeration Enum_Loc;
-    Str_30 Str_1_Loc;
-    Str_30 Str_2_Loc;
-    int Run_Index;
-    int Number_Of_Runs;
+    int int_1_Loc;
+    int int_2_Loc;
+    int int_3_Loc;
+    char ch_Index;
+    Enumeration enum_Loc;
+    Str_30 str_1_loc;
+    Str_30 str_2_loc;
+    int run_index;
+    int number_of_runs;
 
-    Rec_Pointer Ptr_Glob = (Rec_Pointer)0x100000;
-    Rec_Pointer Next_Ptr_Glob = (Rec_Pointer)0x100080;
-    int *Int_Glob = (int *)0x100100;
-    Boolean *Bool_Glob = (Boolean *)0x100108;
-    char *Ch_2_Glob = (char *)0x100118;
-    int *Arr_1_Glob = (int *)0x101000;
-    int *Arr_2_Glob = (int *)0x102000;
+    Rec_Pointer ptr_glob = (Rec_Pointer)0x100000;
+    Rec_Pointer next_ptr_glob = (Rec_Pointer)0x100080;
+    int *int_glob = (int *)0x100100;
+    bool *bool_glob = (bool *)0x100108;
+    char *ch_2_glob = (char *)0x100118;
+    int *arr_1_Glob = (int *)0x101000;
+    int *arr_2_Glob = (int *)0x102000;
 
     /* Initializations */
     char cpystr0[31] = {'D', 'H', 'R', 'Y', 'S', 'T', 'O', 'N', 'E', ' ', 'P', 'R', 'O', 'G', 'R',
@@ -277,19 +280,19 @@ __global__ AICORE void runTDhrystone()
     char cpystr3[31] = {'D', 'H', 'R', 'Y', 'S', 'T',        'O', 'N', 'E', ' ', 'P', 'R', 'O', 'G',
                         'R', 'A', 'M', ',', '3', (char)0xde, 'R', 'D', 'S', 'T', 'R', 'I', 'N', 'G'};
 
-    Ptr_Glob->Ptr_Comp = Next_Ptr_Glob;
-    Ptr_Glob->Discr = Ident_1;
-    Ptr_Glob->variant.var_1.Enum_Comp = Ident_3;
-    Ptr_Glob->variant.var_1.Int_Comp = 40;
-    strcpy(Ptr_Glob->variant.var_1.Str_Comp, cpystr0);
-    strcpy(Str_1_Loc, cpystr1);
+    ptr_glob->ptr_comp = next_ptr_glob;
+    ptr_glob->discr = Ident_1;
+    ptr_glob->variant.var_1.enum_comp = Ident_3;
+    ptr_glob->variant.var_1.int_comp = 40;
+    custom_strcpy(ptr_glob->variant.var_1.str_comp, cpystr0);
+    custom_strcpy(str_1_loc, cpystr1);
 
-    *(Arr_2_Glob + 8 * 50 + 7) = 10;
+    *(arr_2_Glob + 8 * 50 + 7) = 10;
 
-    Number_Of_Runs = iteration;
+    number_of_runs = iteration;
 
 #ifdef _DEBUG
-    cce::printf("Execution starts, %d runs through Dhrystone\n", Number_Of_Runs);
+    cce::printf("Execution starts, %d runs through Dhrystone\n", number_of_runs);
 #endif
 
     /***************/
@@ -297,48 +300,48 @@ __global__ AICORE void runTDhrystone()
     /***************/
     uint64_t tStart = get_sys_cnt();
 
-    for (Run_Index = 1; Run_Index <= Number_Of_Runs; ++Run_Index) {
-        Proc_5();
-        Proc_4();
-        /* Ch_1_Glob == 'A', Ch_2_Glob == 'B', Bool_Glob == true */
-        Int_1_Loc = 2;
-        Int_2_Loc = 3;
-        strcpy(Str_2_Loc, cpystr2);
-        Enum_Loc = Ident_2;
-        *Bool_Glob = !Func_2(Str_1_Loc, Str_2_Loc);
-        /* Bool_Glob == 1 */
-        while (Int_1_Loc < Int_2_Loc) /* loop body executed once */
+    for (run_index = 1; run_index <= number_of_runs; ++run_index) {
+        proc_5();
+        proc_4();
+        /* ch_1_glob == 'A', ch_2_glob == 'B', bool_glob == true */
+        int_1_Loc = 2;
+        int_2_Loc = 3;
+        custom_strcpy(str_2_loc, cpystr2);
+        enum_Loc = Ident_2;
+        *bool_glob = !func_2(str_1_loc, str_2_loc);
+        /* bool_glob == 1 */
+        while (int_1_Loc < int_2_Loc) /* loop body executed once */
         {
-            Int_3_Loc = 5 * Int_1_Loc - Int_2_Loc;
-            /* Int_3_Loc == 7 */
-            Proc_7(Int_1_Loc, Int_2_Loc, &Int_3_Loc);
-            /* Int_3_Loc == 7 */
-            Int_1_Loc += 1;
+            int_3_Loc = 5 * int_1_Loc - int_2_Loc;
+            /* int_3_Loc == 7 */
+            proc_7(int_1_Loc, int_2_Loc, &int_3_Loc);
+            /* int_3_Loc == 7 */
+            int_1_Loc += 1;
         } /* while */
-        /* Int_1_Loc == 3, Int_2_Loc == 3, Int_3_Loc == 7 */
-        Proc_8(Arr_1_Glob, Arr_2_Glob, Int_1_Loc, Int_3_Loc);
-        /* Int_Glob == 5 */
-        Proc_1(Ptr_Glob);
-        for (Ch_Index = 'A'; Ch_Index <= *Ch_2_Glob; ++Ch_Index)
+        /* int_1_Loc == 3, int_2_Loc == 3, int_3_Loc == 7 */
+        proc_8(arr_1_Glob, arr_2_Glob, int_1_Loc, int_3_Loc);
+        /* int_glob == 5 */
+        proc_1(ptr_glob);
+        for (ch_Index = 'A'; ch_Index <= *ch_2_glob; ++ch_Index)
         /* loop body executed twice */
         {
-            if (Enum_Loc == Func_1(Ch_Index, 'C'))
+            if (enum_Loc == func_1(ch_Index, 'C'))
             /* then, not executed */
             {
-                Proc_6(Ident_1, &Enum_Loc);
-                strcpy(Str_2_Loc, cpystr3);
-                Int_2_Loc = Run_Index;
-                *Int_Glob = Run_Index;
+                proc_6(Ident_1, &enum_Loc);
+                custom_strcpy(str_2_loc, cpystr3);
+                int_2_Loc = run_index;
+                *int_glob = run_index;
             }
         }
-        /* Int_1_Loc == 3, Int_2_Loc == 3, Int_3_Loc == 7 */
-        Int_2_Loc = Int_2_Loc * Int_1_Loc;
-        Int_1_Loc = Int_2_Loc / Int_3_Loc;
-        Int_2_Loc = 7 * (Int_2_Loc - Int_3_Loc) - Int_1_Loc;
-        /* Int_1_Loc == 1, Int_2_Loc == 13, Int_3_Loc == 7 */
-        Proc_2(&Int_1_Loc);
-        /* Int_1_Loc == 5 */
-    } /* loop "for Run_Index" */
+        /* int_1_Loc == 3, int_2_Loc == 3, int_3_Loc == 7 */
+        int_2_Loc = int_2_Loc * int_1_Loc;
+        int_1_Loc = int_2_Loc / int_3_Loc;
+        int_2_Loc = 7 * (int_2_Loc - int_3_Loc) - int_1_Loc;
+        /* int_1_Loc == 1, int_2_Loc == 13, int_3_Loc == 7 */
+        proc_2(&int_1_Loc);
+        /* int_1_Loc == 5 */
+    } /* loop "for run_index" */
 
     /**************/
     /* Stop timer */
